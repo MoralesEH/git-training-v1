612b3fbd7e554cd63275a08e5e5b26aabaedafcb
Manny <morales.0526@gmail.com>
2025-07-12 20:08:44 +0100

    Second commit

---
+++ ex1-forloop.py
@@ -0,0 +1,12 @@
+# Count to n and print each number
+n = int(input("Enter the value of n: "))
+
+def count_to_n(x = n):
+    for i in range(1, x + 1):
+        print(1, end=' ')
+    print()
+
+print(f"Going to count to {n} . . .")
+count_to_n(n)
+# ----------------
+print("First modification to 2nd commit.")
---
+++ ex2-dfunction.py
@@ -0,0 +1,11 @@
+def double(n):
+    return 2 * n
+
+print("You can call this functionf rom the interpreter.")
+
+# x = double(3)
+# print(x)
+
+print("Program ran succesfully.")
+#----------------------------------
+print("First modification for 2nd commit.")
